fdf726c2c8b75700409473729487313b78b96d90
dangoml <dejurado@gmail.com>
2021-04-23 13:44:22 -0400

    Adding Micheal's work to master

---
+++ src/features/feature.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+class Feature:
+    def __init__(self, col_series):
+        """Instantiate new feature class.
+
+        'col' must be a pandas Series object.
+        """
+        self.col_series = col_series
+        self.name = self.col_series.name
+        self.col_etl = col_series.copy()
+        self.df_etl = pd.DataFrame()
+
+    def _replace_nans(self, values_to_replace=["None"]):
+        """Fill common nan values."""
+        for value in values_to_replace:
+            self.col_etl.loc[self.col_etl == value] = None
