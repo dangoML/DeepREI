5cf34a653025f4bf4e22559b6ec265fbb2e9e5dc
Daniel Jurado <dejurado@gmail.com>
2021-05-18 20:18:54 -0700

    Folder Cleanup

--- Model/src/preprocessing/DatasetBuilder.py
+++ Model/src/preprocessing/DatasetBuilder.py
@@ -10,21 +10,12 @@ from src.features.categorical.CategoricalValueNullBinaryNominalCreator import Ca
 class DatasetBuilder(DatasetPreprocessor):
     def __init__(self, dataset, target_var='', cont_num_columns=[], discrete_num_columns=[], nominal_cat_columns=[], verbose_columns=[], verbose_threshold=[], verbose_most_common=[]):
         """PreProcess Training and Testing Data."""
-        # DatasetPreprocessor.__init__(self, dataset)
-        # TargetVariableCreator.__init__(self, target_var)
-        # NumericValueNullCreator.__init__(
-        #     self, cont_num_columns, discrete_num_columns)
-        # CategoricalValueNullBinaryNominalCreator.__init__(
-        #     self, nominal_cat_columns)
-        # VerboseValueNullBinaryCreator.__init__(
-        #     self, verbose_columns, verbose_threshold, verbose_most_common)
-
         super().__init__(dataset, target_var, cont_num_columns, discrete_num_columns,
                          nominal_cat_columns, verbose_columns, verbose_threshold, verbose_most_common)
 
     def build_model_input(self):
         """Run ETL of the column."""
-        # self._create_target_variable()
+        self._create_target_variable()
         self._create_numeric_features()
         # self._create_verbose_features()
         # self._create_custom_categorical_nominal_features()
