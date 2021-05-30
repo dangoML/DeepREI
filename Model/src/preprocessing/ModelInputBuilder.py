53371f23c6b300cc273070cbff82c2a71d8846ef
Daniel Jurado <dejurado@gmail.com>
2021-05-30 14:27:30 -0400

    Hotfixes for colab

--- Model/src/preprocessing/ModelInputBuilder.py
+++ Model/src/preprocessing/ModelInputBuilder.py
@@ -2,9 +2,10 @@ from src.preprocessing.ModelInputPCA import ModelInputPCA
 
 import pandas as pd
 
+
 class ModelInputBuilder(ModelInputPCA):
-    def __init__(self, dataset, target_var='', cont_num_columns=[], discrete_num_columns=[], nominal_cat_columns=[], 
-                        verbose_columns=[], verbose_threshold=[], verbose_most_common=[], pca_columns=[], pca_expl_var=.95):
+    def __init__(self, dataset, target_var='', cont_num_columns=[], discrete_num_columns=[], nominal_cat_columns=[],
+                 verbose_columns=[], verbose_threshold=[], verbose_most_common=[], pca_columns=[], pca_expl_var=.95):
         """Instantiate Model Input Table."""
         super().__init__(dataset, target_var, cont_num_columns, discrete_num_columns,
                          nominal_cat_columns, verbose_columns, verbose_threshold, verbose_most_common, pca_columns, pca_expl_var)
@@ -15,5 +16,5 @@ class ModelInputBuilder(ModelInputPCA):
         self._etl_dataset()
         self._feature_limit_filters()
         self._train_valid_test_split()
-        # self._scale_train_valid_test()
-        # self._pca_transform()
+        self._scale_train_valid_test()
+        self._pca_transform()
